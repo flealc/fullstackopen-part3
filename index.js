4143cc5337b9d3e9ebb23ae307673d2a70b84645
Fidel Leal <flus1717@gmail.com>
2023-04-09 11:36:02 -0500

    add phonebook database step2

--- index.js
+++ index.js
@@ -30,28 +30,6 @@ app.use(morgan(function (tokens, req, res) {
 app.use(express.json())
 app.use(express.static('build'))
 
-/* let persons = [
-        { 
-          "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
-        },
-        { 
-          "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
-        },
-        { 
-          "id": 3,
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
-        },
-        { 
-          "id": 4,
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
-] */
 
 app.get('/', (request, response) => {
     response.send('<h1>Ring Ring</h1>')
@@ -92,7 +70,6 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body 
-    const newId =  Math.floor(Math.random() * (5, 5000) + 5)
     
     if (!body.name || !body.number) {
         return response.status(400).json({
@@ -100,20 +77,15 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (persons.find(p => p.name === body.name)) {
-        return response.status(400).json({
-            error: "name already exists in phonebook"
-        })
-    }
-
-    const person = {
-        id: newId,
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+
 
 })
 
