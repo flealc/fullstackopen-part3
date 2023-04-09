48c65f0b0db255055b0385ed6b0997f95e2253cf
Fidel Leal <flus1717@gmail.com>
2023-04-09 15:31:45 -0500

    add phonebook database step5

--- index.js
+++ index.js
@@ -102,8 +102,22 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+})
 
+app.put('/api/persons/:id', (request, response, next) => {
+    console.log('found me')
+    const body = request.body
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
 
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
